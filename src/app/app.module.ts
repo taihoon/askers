8d3aab979c807bbcdd9dd92c657203c8593477a9
jack <kishu@navercorp.com>
2019-08-29 22:47:18 +0900

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AuthModule } from '@app/modules/auth/auth.module';
 import { ChannelModule } from '@app/modules/channel/channel.module';
-import { AppComponent } from '@app/app.component';
-
 import { AppInitService } from '@app/core/app-init.service';
+import { AppComponent } from '@app/app.component';
 import { HomeComponent } from '@app/modules/home/home.component';
-import { environment } from '../environments/environment';
 
 export function init(appInitService: AppInitService) {
   return () => appInitService.init();
@@ -20,12 +21,14 @@ export function init(appInitService: AppInitService) {
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase, 'web'),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    AppRoutingModule,
     AuthModule,
     ChannelModule,
-    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
--- src/app/modules/channel/channel.module.ts
+++ src/app/modules/channel/channel.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChannelRoutingModule } from './channel-routing.module';
 import { ChannelFormComponent } from './channel-form/channel-form.component';
 import { ChannelCreateComponent } from './channel-create/channel-create.component';
 import { ChannelListComponent } from './channel-list/channel-list.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
